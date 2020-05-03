978ad29a2b376268d1fa2e4569d9e4f9673f3385
RohanGautam <rohan1gautam@gmail.com>
2020-05-03 13:55:19 +0800

    added power of 2 stuff

--- playground.py
+++ playground.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import LabelBinarizer
 from math import sqrt, pi
 import numpy as np
 import cv2
+import math
 
 info = "rohan"
 org = cv2.imread('./img/bruh.jpg')
@@ -13,18 +14,29 @@ img = cv2.cvtColor(org, cv2.COLOR_BGR2GRAY)
 row1 = img[0]
 
 
+def closestPowerOfTwo(n):
+    return 2**math.ceil(math.log(n)/math.log(2))
+
+def highestPowerOfTwo(img):
+    '''highest power of two amongst all rows'''
+    maxVal = max([closestPowerOfTwo(len(list(set(row)))) for row in img])
+    return (maxVal, int(math.log(maxVal,2)))
+
+powerVal, exponent = highestPowerOfTwo(img)
 # testing with dummy row
-r = [20,22,23,43,43,123,7,8,9]
+# r = [20,22,23,43,43,123,7,8,9]
 # one-hot encode them -> convert them to binary vectors
+# why? because sum of amplitudes we pass should be = 1
 lb = LabelBinarizer()
-r_oneHot = lb.fit_transform(r)
+r_oneHot = lb.fit_transform(row1)
+
 
 # qbits = []
 # # create qbits
 circuits = []
 for vector in r_oneHot:
-    qBitNum = 3
-    qc = QuantumCircuit(qBitNum) # for vector of size 2^3=8
+    qBitNum = exponent
+    qc = QuantumCircuit(qBitNum) 
     initial_state = vector   # Define initial_state as |1>. sum of amplitudes = 1, has to be power of 2
     qc.initialize(initial_state, list(range(qBitNum))) # Apply initialisation operation to the 0th qubit
     # qc.draw()
@@ -45,4 +57,4 @@ for circuit in circuits:
     retrievedVectors.append(getVector(circuit))
 retrievedVectors = np.array(retrievedVectors) # convert to numpy array
 print(retrievedVectors)
-lb.inverse_transform(retrievedVectors)
+lb.inverse_transform(retrievedVectors)# the final vector
