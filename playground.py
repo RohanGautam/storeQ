03b6d1c3943388b419d3e498dd25d5a2a7f30e1e
RohanGautam <rohan1gautam@gmail.com>
2020-05-03 11:05:11 +0800

    infp transformation and retrieval from q cicuits

--- playground.py
+++ playground.py
@@ -1,8 +1,10 @@
 import qiskit as qk
 from qiskit import QuantumCircuit, execute, Aer
 from qiskit.visualization import plot_histogram, plot_bloch_vector
-from math import sqrt, pi
+
 from sklearn.preprocessing import LabelBinarizer
+from math import sqrt, pi
+import numpy as np
 import cv2
 
 info = "rohan"
@@ -12,28 +14,35 @@ row1 = img[0]
 
 
 # testing with dummy row
-r = [20,22,23,43,43,123,7,8]
+r = [20,22,23,43,43,123,7,8,9]
 # one-hot encode them -> convert them to binary vectors
 lb = LabelBinarizer()
-lb.fit_transform(r)
+r_oneHot = lb.fit_transform(r)
 
-qbits = []
-# create qbits
+# qbits = []
+# # create qbits
+circuits = []
+for vector in r_oneHot:
+    qBitNum = 3
+    qc = QuantumCircuit(qBitNum) # for vector of size 2^3=8
+    initial_state = vector   # Define initial_state as |1>. sum of amplitudes = 1, has to be power of 2
+    qc.initialize(initial_state, list(range(qBitNum))) # Apply initialisation operation to the 0th qubit
+    # qc.draw()
+    circuits.append(qc)
 
 
-
-qc = QuantumCircuit(2)
-initial_state = [0,1,0,0]   # Define initial_state as |1>. sum of amplitudes = 1, has to be power of 2
-qc.initialize(initial_state, [0,1]) # Apply initialisation operation to the 0th qubit
-qc.draw()
 
 backend = Aer.get_backend('statevector_simulator')
-result = execute(qc,backend).result()
-out_state = result.get_statevector()
-print(out_state)
-# out_state.real is the real number part
+def getVector(qc):
+    result = execute(qc,backend).result()
+    out_state = result.get_statevector()
+    # print(out_state)
+    vector = [int(x.real) for x in out_state]
+    return vector
 
-# Creating Qubits
-q = qk.QuantumRegister(2)
-# Creating Classical Bits
-c = qk.ClassicalRegister(2)
+retrievedVectors=[]
+for circuit in circuits:
+    retrievedVectors.append(getVector(circuit))
+retrievedVectors = np.array(retrievedVectors) # convert to numpy array
+print(retrievedVectors)
+lb.inverse_transform(retrievedVectors)
